4b6120bb2a59e4141b31b95445e1feb658e17d75
REINALDO FERNANDES <reinaldo.f.menezes@outlook.com>
2024-09-07 13:44:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 function pesquisar() {
-    // Obtém a seção onde os resultados da pesquisa serão exibidos
-    // pelo seu ID e armazena em uma variável.
     let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
   
-    // Loga os dados no console para fins de depuração. 
-    // Isso é útil durante o desenvolvimento para verificar se os dados estão sendo carregados corretamente.
-    console.log(dados);
-  
-    // Inicializa uma string vazia para armazenar o HTML dos resultados.
-    // Essa string será preenchida com os elementos HTML gerados.
-    let resultados = "";
-  
-    // Itera sobre cada item (personagem) no array de dados.
-    // Para cada item, cria um novo elemento HTML e o adiciona à string 'resultados'.
-    for (let dado of dados) {
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descrição}</p>
-          <a href="${dado.link}" target="_blank">Saiba mais</a>
-        </div>
-      `;
+    if (campoPesquisa === "") {
+        section.innerHTML = "<p>Nada foi encontrado.</p>";
+        return;
     }
   
-    // Atribui o conteúdo da string 'resultados' ao HTML interno da seção.
-    // Isso substitui o conteúdo anterior da seção pelos novos elementos HTML.
+    campoPesquisa = campoPesquisa.toLowerCase();
+    let resultados = "";
+  
+    for (let dado of dados) {
+        let titulo = dado.titulo.toLowerCase();
+        let descricao = dado.descrição.toLowerCase();
+        let tags = dado.tags.toLowerCase();
+  
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descrição}</p>
+                <a href="${dado.link}" target="_blank">Saiba mais</a>
+            </div>`;
+        }
+    }
+  
+    if (resultados === "") {
+        resultados = "<p>Personagem não encontrado ou não faz parte da família principal.</p>";
+    }
+  
     section.innerHTML = resultados;
   }
+  
+  function outrosPersonagens() {
+    let section = document.getElementById("resultados-pesquisa");
+    let outros = dados.find(dado => dado.titulo.toLowerCase() === "outros personagens");
+  
+    if (outros) {
+        section.innerHTML = `
+        <div class="item-resultado">
+            <h2>${outros.titulo}</h2>
+            <p class="descricao-meta">${outros.descrição}</p>
+            <a href="${outros.link}" target="_blank">Saiba mais</a>
+        </div>`;
+    } else {
+        section.innerHTML = "<p>Outros personagens não encontrados.</p>";
+    }
+  }
+  
