910ce20689876a7585829491005fdfa60782a72f
REINALDO FERNANDES <reinaldo.f.menezes@outlook.com>
2024-09-07 13:28:56 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,28 +3,39 @@ let dados = [
     {
         titulo: "Homer Simpson",
         descrição: "O pai da família, trabalha na Usina Nuclear de Springfield e é conhecido por sua preguiça, amor por donuts e cerveja Duff",
-        link: "https://pt.wikipedia.org/wiki/Homer_Simpson"
+        link: "https://pt.wikipedia.org/wiki/Homer_Simpson",
+        tags:"Pai  Bêbado Careca"
     },
     {
         titulo: "Marge Simpson",
         descrição: "A mãe da família, uma dona de casa dedicada e paciente, com um penteado azul icônico.",
-        link: "https://pt.wikipedia.org/wiki/Marge_Simpson"
+        link: "https://pt.wikipedia.org/wiki/Marge_Simpson",
+        tags: "Mãe Cabelo azul dona de casa"
     },
     {
         titulo: "Bart Simpson",
         descrição: "O filho mais velho, um menino travesso e inteligente, sempre aprontando.",
-        link: "https://pt.wikipedia.org/wiki/Bart_Simpson"
-
+        link: "https://pt.wikipedia.org/wiki/Bart_Simpson",
+        tags:"El Barto menino pestinha trabalhoso skatista"
 
     },
     {
         titulo: "Lisa Simpson",
         descrição: "A filha do meio, uma garota brilhante, vegetariana e saxofonista.",
-        link: "https://pt.wikipedia.org/wiki/Lisa_Simpson"
+        link: "https://pt.wikipedia.org/wiki/Lisa_Simpson",
+        tags:"Nerd Saxofonista vegetariana do meio"
     },
     {
         titulo: "Maggie Simpson",
         descrição: "A caçula, uma bebê que se comunica apenas através de sua chupeta.",
-        link: "https://pt.wikipedia.org/wiki/Maggie_Simpson"
+        link: "https://pt.wikipedia.org/wiki/Maggie_Simpson",
+        tags:"Bebê mais nova chupeta"
+    },
+
+    {
+        titulo: "Outros Personagens",
+        descrição: "Click em saiba mais para ver outros personagens da obra!",
+        link: "https://pt.wikipedia.org/wiki/Lista_de_personagens_de_The_Simpsons",
+        tags:"personagem"
     }
 ];
